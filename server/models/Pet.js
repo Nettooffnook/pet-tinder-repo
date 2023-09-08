b03dc7faa9bc333caa7e5a6a5bb2631d0a51947f
alexeflorin <goremic@yahoo.com>
2023-09-07 23:12:16 -0400

    models, schema, utils, typeDefs and resolvers

---
+++ server/models/Pet.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const petSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+
+  picture: {
+    type: String,
+  },
+  breed: {
+    type: String,
+    required: true
+  },
+
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+  },
+  userLikes: [
+    {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }
+  ],
+  userDislikes: [
+    {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }
+  ],
+  matches: [
+    {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }
+  ],
+
+});
+
+const Pet = mongoose.model('Pet', petSchema);
+
+module.exports = Pet;

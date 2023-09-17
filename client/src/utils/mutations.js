69781ea2f1144b4c71882a582dddc6794cd74e26
HyonNeo <128829684+HyonNeo@users.noreply.github.com>
2023-09-17 16:21:28 -0400

    fixed error of page didnt rendering

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,6 +1,6 @@
 import { gql } from '@apollo/client';
 
-export const QUERY_CREATEUSER = gql`
+export const CREATE_USER = gql`
   mutation addUser($username: String!, $email: String!, $password: String!) {
     addUser(username: $username, email: $email, password: $password) {
       username
@@ -15,7 +15,7 @@ export const QUERY_CREATEUSER = gql`
   }
 `;
 
-export const QUERY_CREATEPET = gql`
+export const CREATE_PET = gql`
   mutation addPet($namePet: String!, $picturePet: String!, $breedPet: String!, $ownerPet: String!) {
     addPet(namePet: $namePet, picturePet: $picturePet, breedPet: $breedPet, ownerPet: $ownerPet) {
       namePet
@@ -25,5 +25,12 @@ export const QUERY_CREATEPET = gql`
     }
   }
 `;
+
+export const CREATE_MATCHUP = gql `
+ mutation createMatchup{
+  userID
+  petId
+ }
+`
 
 
